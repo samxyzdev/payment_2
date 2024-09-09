a9d003857cefec39337bafd30f0dafe2a0ab1d1f
Sameer Ahmed <sameer937655@gmail.com>
2024-09-09 22:46:21 +0530

    add signup signin dashbora and appbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Auth } from "./pages/Auth";
+import { Dashboard } from "./pages/Dashboard";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/signup" element={<Auth type="signup" />} />
           <Route path="/signin" element={<Auth type="signin" />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
         </Routes>
       </BrowserRouter>
     </>
